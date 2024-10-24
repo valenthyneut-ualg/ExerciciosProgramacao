428057047771a714470008b2d757715d9a6f7e28
Valentim Uritu Tokalenko <a90668@ualg.pt>
2024-10-24 18:11:36 +0100

    Exercício 4

---
+++ exercicios/AulaTI2,STI2/Exercicio4/main.py
@@ -0,0 +1,14 @@
+from math import pi
+
+if __name__ == "__main__":
+    while True:
+        radius = input("Escreva o raio do círculo (STOP para parar): ")
+        if radius == "STOP": break
+
+        try:
+            radius = float(radius)
+            perimeter = 2 * pi * radius
+            area = pi * radius ** 2
+            print(f'Perímetro: {perimeter}; Área do círculo: {area};')
+        except ValueError:
+            print("Raio inválido!")
