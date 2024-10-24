acd48215e912954d4105f8d4fa4a403bec7c0dfa
Valentim Uritu Tokalenko <a90668@ualg.pt>
2024-10-24 18:27:59 +0100

    Adicionar verificação ao Exc. 4

--- exercicios/AulaTI2,STI2/Exercicio4/main.py
+++ exercicios/AulaTI2,STI2/Exercicio4/main.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":
 
         try:
             radius = float(radius)
+            if radius <= 0: raise ValueError
+
             perimeter = 2 * pi * radius
             area = pi * radius ** 2
             print(f'Perímetro: {perimeter}; Área do círculo: {area};')
         except ValueError:
-            print("Raio inválido!")
+            print("Raio inválido! (Este programa apenas aceita números positivos com/sem casas decimais)")
