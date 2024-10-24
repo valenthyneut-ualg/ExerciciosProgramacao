c333f8b88c8fcda58f3c05408e125b2558db65d6
Valentim Uritu Tokalenko <a90668@ualg.pt>
2024-10-24 14:52:13 +0100

    Exercício 3

---
+++ exercicios/AulaTI2,STI2/Exercicio3/main.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    while True:
+        euroAmount = input("Escreva uma quantidade de dinheiro em euros (STOP para parar): ")
+        if euroAmount == "STOP": break
+
+        try:
+            euroAmount = float(euroAmount)
+            if euroAmount > 0:
+                print(f'{euroAmount}€ em dolares: {(euroAmount * 1.17):.4f}$')
+            else:
+                print("Quantidade inválida!")
+        except ValueError:
+            print("Quantidade inválida!")
