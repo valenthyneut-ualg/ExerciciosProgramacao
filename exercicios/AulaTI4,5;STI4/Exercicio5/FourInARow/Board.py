28932ece7c7228c6ba76ea71b5e5744bf9f67fa4
Valentim Uritu Tokalenko <a90668@ualg.pt>
2024-11-18 14:47:43 +0000

    TI4,5;STI4; Ex.5 - Playing

--- exercicios/AulaTI4,5;STI4/Exercicio5/FourInARow/Board.py
+++ exercicios/AulaTI4,5;STI4/Exercicio5/FourInARow/Board.py
@@ -24,3 +24,14 @@ class Board:
         for i in range(8):
             numString += f' {i + 1}  '
         print(numString)
+
+    def play(self, player: str, column: int):
+        if column <= 0 or column > 8: raise ValueError("Nº da coluna tem de estar entre 1 e 8. (inclusivo)");
+        trueColumn = column - 1
+
+        lastEmptyRow = -1
+        for i in range(8):
+            if self.state[i][trueColumn].strip() == "": lastEmptyRow = i
+
+        if lastEmptyRow == -1: raise ValueError("A coluna já está preenchida.")
+        self.state[lastEmptyRow][trueColumn] = player
--- exercicios/AulaTI4,5;STI4/Exercicio5/main.py
+++ exercicios/AulaTI4,5;STI4/Exercicio5/main.py
@@ -2,4 +2,8 @@ from FourInARow.Board import Board
 
 if __name__ == "__main__":
     board = Board()
+    board.play("X", 1)
+    board.play("O", 1)
+    board.play("X", 2)
+    board.play("O", 7)
     board.display()
