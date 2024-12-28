ac337710d73af25375716fe661d1ddc6ea9ae6f4
Valentim Uritu Tokalenko <a90668@ualg.pt>
2024-12-28 16:10:06 +0000

    TI7,STI6; Basic gameplay flow

--- exercicios/AulaTI7,STI6/Glory/Controller.py
+++ exercicios/AulaTI7,STI6/Glory/Controller.py
@@ -1,4 +1,5 @@
 from random import randrange
+from time import sleep
 
 from .Board import Board
 
@@ -21,8 +22,43 @@ class Controller:
         sortedOrder = sorted(turnOrder.items(), key=lambda x: x[1])
         return sortedOrder
 
+    def turn(self, player: int):
+        sleep(2)
+
+        print()
+        print()
+
+        self.board.display()
+        manipulate = input(f'Jogador {player + 1}, carregue no Enter para atirar os dados. ')
+        try: manipulate = int(manipulate)
+        except ValueError: pass
+
+        if type(manipulate) == int:
+            self.board.move(player, manipulate)
+            print(f'Rodada manipulada.')
+        else:
+            rolls = [randrange(1, 6), randrange(1, 6)]
+            rollSum = rolls[0] + rolls[1]
+            self.board.move(player, rollSum)
+            sleep(0.5)
+            print()
+            print()
+            print(f'Resultado: [{rolls[0]}][{rolls[1]}] = {rollSum}')
+
+        print(f'Jogador {player + 1}, chegou à posição {self.board.playerPositions[player]}!')
+
     def start(self):
         self.turnOrder = self.rollTurnOrder(2)
         playerOrder = ", ".join(map(lambda x: str(x[0] + 1), self.turnOrder))
 
         print("Ordem dos jogadores:", playerOrder)
+
+        player = 0
+        while True:
+            self.turn(self.turnOrder[player][0])
+            if 64 in self.board.playerPositions: break
+
+            player += 1
+            if player > len(self.turnOrder) - 1: player = 0
+
+        print(f'Jogador {self.turnOrder[player][0] + 1}, ganhou!')
