4bd04d03ba5d1dc7d803c5e6f4ad7f94e5c1949d
valenthyne-ut <valenthyne.ut@gmail.com>
2025-01-03 18:10:01 +0000

    TI7,STI6; Selecting games

--- exercicios/AulaTI7,STI6/main.py
+++ exercicios/AulaTI7,STI6/main.py
@@ -1,5 +1,21 @@
-from Games.Glory.Controller import Controller
+from Games.Glory.Controller import Controller as GloryController
+from Games.AbstractController import AbstractController
 
 if __name__ == "__main__":
-    controller = Controller(3)
-    controller.start()
+	controllers: tuple[AbstractController | None] = (None, None, GloryController(2))
+	controller = None
+
+	while controller == None:
+		print("Escolha um jogo dos seguintes três:")
+		print("1 - Jogo do Galo")
+		print("2 - 4 em linha")
+		print("3 - Jogo da Glória")
+
+		try:
+			gameChoice = int(input("\n")) - 1
+			if gameChoice in range(0, 3): controller = controllers[gameChoice]
+			else: raise ValueError("\nJogo inválido.\n")
+		except ValueError as error:
+			print(error)
+
+	if isinstance(controller, AbstractController): controller.start()
